0caa359bbd0e9e53443ab4fc12a0d9bd21a35053
Soumyadeepaul <paulsoumyadeep7@gmail.com>
2026-07-14 00:46:02 +0530

    Session service created

--- backend/app/services/session/session_service.py
+++ backend/app/services/session/session_service.py
@@ -1,1 +1,21 @@
 # Handles session lifecycle.
+import random
+import string
+
+class Session:
+    __activeSession= set()
+    
+    def __generateId(self):
+        length = 10
+        return ''.join(random.choices(string.ascii_letters + string.punctuation + string.digits, k=length))
+        
+    def createSession(self):
+        id=self.__generateId()
+        while id in self.__activeSession:
+            id=self.__generateId()
+        self.__activeSession.add(id)
+        return id
+    
+    def endSession(self, sessionId):
+        self.__activeSession.discard(sessionId)
+        
